43a9af351b95d5207f304d9977db3ad614d64da6
Malik <mrogers@icstars.org>
2026-02-18 02:39:07 -0600

    My First Calc

---
+++ script.js
@@ -0,0 +1,31 @@
+function calculate(){
+
+let num1 = parseFloat(document.getElementById("num1").value);
+let num2 = parseFloat(document.getElementById("num2").value);
+let opertation = document.getElementById("operation").value;
+let result;
+
+
+// Logic behind the calculator
+
+// make sure the user has entered numbers
+
+if(isNaN(num1) || isNaN(num2)){
+    result = "please enter an actual number fool."
+}else{
+
+
+switch 
+}
+
+
+
+
+
+
+
+
+
+
+
+}
