d58cff14dc751b549bf2e17d41b69c5313e9dc7f
Malik <mrogers@icstars.org>
2026-02-21 14:32:07 -0600

    Overhauled the colors almost done

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
-function calculate(){
+const display = document.getElementById("display");
 
-let num1 = parseFloat(document.getElementById("num1").value);
-let num2 = parseFloat(document.getElementById("num2").value);
-let opertation = document.getElementById("operation").value;
-let result;
-
-
-// Logic behind the calculator
-
-// make sure the user has entered numbers
-
-if(isNaN(num1) || isNaN(num2)){
-    result = "please enter an actual number fool."
-}else{
-
-
-switch 
+function appendTodisplay(input){
+    display.value += input;
 }
 
+function cleardisplay(){
+    display.value = "";
+}
 
-
-
-
-
-
-
-
-
-
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(Error){
+        display.value = "Error";
+    }
 }
